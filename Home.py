f4bfb50c8200a2ff3e57727eb9db41dc0b69e6bb
Max16 <max16@naver.com>
2024-02-01 13:43:47 +0900

    7.2 Data Flow of Streamlit

--- Home.py
+++ Home.py
@@ -1,9 +1,29 @@
 import streamlit as st
+from datetime import datetime
 
-st.title("Hello Langchain 👋")
-st.subheader("Welcome to Streamlit")
-st.markdown(
-    """
-    #### I love it!
-    """
+today = datetime.today().strftime("%H:%M:%S")
+
+st.title(today)
+
+model = st.selectbox(
+    "Choose your LLM",
+    (
+        "GPT-3.5",
+        "GPT-4",
+    ),
 )
+
+if model == "GPT-3.5":
+    st.write("cheap")
+else:
+    st.write("not cheap")
+    name = st.text_input("What is your name?")
+    st.write(name)
+
+    value = st.slider(
+        "templature",
+        min_value=0.0,
+        max_value=1.0,
+    )
+
+    st.write(value)
