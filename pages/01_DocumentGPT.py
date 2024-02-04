15fb8ba87ee549b65f43631aae6541617e394ed0
Max16 <max16@naver.com>
2024-02-04 21:45:05 +0900

    7.7 Chat History

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -11,26 +11,37 @@ st.set_page_config(
 )
 
 
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
     cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
-
     splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=500,
         chunk_overlap=100,
     )
-
-    loader = UnstructuredFileLoader(f"./.cache/files/{file.name}")
+    loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     embeddings = OpenAIEmbeddings()
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vector_store = FAISS.from_documents(docs, cached_embeddings)
     retriever = vector_store.as_retriever()
     return retriever
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
 
 
 st.title("DocumentGPT")
@@ -40,15 +51,24 @@ st.markdown(
     Welcome!
     
     Use this chatbot to ask question to an AI about your files!        
+    
+    Upload your files on the sidebar.
     """
 )
 
-file = st.file_uploader(
-    "Upload a .txt .pdf .md or .docx file",
-    type=["txt", "pdf", ".md", "docx"],
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf .md or .docx file",
+        type=["txt", "pdf", ".md", "docx"],
+    )
 
 if file:
     retriever = embed_file(file)
-    result = retriever.invoke("winston")
-    st.write(result)
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+        send_message(f"You said {message}, right?", "ai")
+else:
+    st.session_state["messages"] = []
