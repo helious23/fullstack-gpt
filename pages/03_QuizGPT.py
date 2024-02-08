189bcb9e2b951ba68aff55b29c05510a5acebfa2
Max16 <max16@naver.com>
2024-02-08 09:32:54 +0900

    WikipediaRetriever

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,3 +1,52 @@
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.retrievers import WikipediaRetriever
+from langchain.text_splitter import CharacterTextSplitter
 import streamlit as st
 
+st.set_page_config(
+    page_title="QuizGPT",
+    page_icon="❓",
+)
+
 st.title("QuizGPT")
+
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=500,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+with st.sidebar:
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .txt .pdf .md or .docx file",
+            type=["txt", "pdf", "md", "docx"],
+        )
+        if file:
+            docs = split_file(file)
+            st.write(docs)
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=5)
+            with st.status("Searching Wikipedia..."):
+                docs = retriever.get_relevant_documents(topic)
