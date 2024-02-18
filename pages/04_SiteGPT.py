678d31cad11db0db814a740f30b47e4429810fd4
Max16 <max16@naver.com>
2024-02-18 11:07:05 +0900

    10.2 SitemapLoader

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,6 +1,18 @@
-from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_loaders import AsyncChromiumLoader, SitemapLoader
 from langchain.document_transformers import Html2TextTransformer
 import streamlit as st
+
+
+@st.cache_data(
+    show_spinner="웹사이트를 읽고 있습니다. 이 작업은 최초 1회만 진행됩니다."
+)
+def load_website(url):
+    loader = SitemapLoader(url)
+    loader.requests_per_second = 1
+    loader.requests_kwargs = {"verify": False}
+    docs = loader.load()
+    return docs
+
 
 st.set_page_config(
     page_title="SiteGPT",
@@ -9,8 +21,7 @@ st.set_page_config(
 
 st.title("SiteGPT")
 
-html2text_transformer = Html2TextTransformer()
-
+html2text_tramsformer = Html2TextTransformer()
 
 st.markdown(
     """
@@ -23,12 +34,19 @@ st.markdown(
 with st.sidebar:
     url = st.text_input(
         "URL 주소를 적어주세요",
-        placeholder="https://example.com",
+        placeholder="https://example.com/sitemap.xml",
     )
 
+# if url:
+#     loader = AsyncChromiumLoader([url])
+#     docs = loader.load()
+#     html2text_tramsformer.transform_documents(docs)
+#     st.markdown(docs)
 
 if url:
-    loader = AsyncChromiumLoader([url])
-    docs = loader.load()
-    transformed = html2text_transformer.transform_documents(docs)
-    st.write(docs)
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("Sitemap URL 을 적어주세요")
+    else:
+        docs = load_website(url)
+        st.write(docs)
