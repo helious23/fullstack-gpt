7fc47a7973bd1ae1bf25dbf4133fbec9bc583d0f
Max16 <max16@naver.com>
2024-03-10 09:29:11 +0900

    12.7 Agent Prompt

--- pages/06_InvestorGPT.py
+++ pages/06_InvestorGPT.py
@@ -1,4 +1,128 @@
+import os
+from typing import Any, Type
+from langchain.agents import AgentType, Tool, initialize_agent
+from langchain.schema import SystemMessage
+from langchain.utilities import DuckDuckGoSearchAPIWrapper
+from langchain.chat_models import ChatOpenAI
+from langchain.tools import BaseTool
+from pydantic import BaseModel, Field
+import requests
 import streamlit as st
+import asyncio
+
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    model_name="gpt-3.5-turbo-0125",
+)
+
+alpha_vantage_api_key = os.environ.get("ALPHA_VANTAGE_API_KEY")
+
+
+class StockMarketSymbolSearchToolArgsSchema(BaseModel):
+    query: str = Field(description="The query you will search for")
+
+
+class StockMarketSymbolSearchTool(BaseTool):
+    name = "StockMarketSymbolSearchTool"
+    description = """
+    Use this tool to fine the stock market symbol for a company.
+    It takes a query as an argument.
+    Example query: Stock market Symbol for Apple Company
+    """
+
+    args_schema: Type[StockMarketSymbolSearchToolArgsSchema] = (
+        StockMarketSymbolSearchToolArgsSchema
+    )
+
+    def _run(self, query):
+        ddg = DuckDuckGoSearchAPIWrapper()
+        return ddg.run(query)
+
+
+class CompanyOverviewArgsSchema(BaseModel):
+    symbol: str = Field(
+        description="Stock symbol of the company. Example:APPL, TSLA",
+    )
+
+
+class CompanyOverviewTool(BaseTool):
+    name = "CompanyOverview"
+    description = """
+    Use this to get an overview of the finalcials of the company.
+    You should enter a stock symbol.
+    """
+    args_schema: Type[CompanyOverviewArgsSchema] = CompanyOverviewArgsSchema
+
+    def _run(self, symbol):
+        r = requests.get(
+            f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey={alpha_vantage_api_key}"
+        )
+        return r.json()
+
+
+class CompanyIncomeStatementTool(BaseTool):
+    name = "CompanyIncomeStatement"
+    description = """
+    Use this to get the income statement of a company.
+    You should enter a stock symbol.
+    """
+    args_schema: Type[CompanyOverviewArgsSchema] = CompanyOverviewArgsSchema
+
+    def _run(self, symbol):
+        r = requests.get(
+            f"https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={alpha_vantage_api_key}"
+        )
+        return r.json()["annualReports"]
+
+
+class CompanyStockPerformanceTool(BaseTool):
+    name = "CompanyStockPerformance"
+    description = """
+    Use this to get the weekly performance of a company stock.
+    You should enter a stock symbol.
+    """
+    args_schema: Type[CompanyOverviewArgsSchema] = CompanyOverviewArgsSchema
+
+    def _run(self, symbol):
+        r = requests.get(
+            f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={symbol}&apikey={alpha_vantage_api_key}"
+        )
+        response = r.json()
+        return list(response["Weekly Time Series"].items)[:200]
+
+
+agent = initialize_agent(
+    llm=llm,
+    verbose=True,
+    agent=AgentType.OPENAI_FUNCTIONS,
+    tools=[
+        StockMarketSymbolSearchTool(),
+        CompanyOverviewTool(),
+        CompanyIncomeStatementTool(),
+        CompanyStockPerformanceTool(),
+    ],
+    agent_kwargs={
+        "system_message": SystemMessage(
+            content="""
+            You are a hedge fund manager for Korean.
+
+            You evaluate a company and provide your opinion and reasons why the stock is a buy or not.
+
+            Consider the performance of a stock, the company overview and the income statement.
+
+            Be assertive in your judgement and recommend the stock or advise the user against it.
+            
+            Please reply in English and translate in Korean.
+            """,
+        )
+    },
+)
+
 
 st.set_page_config(
     page_title="InvestorGPT",
@@ -9,6 +133,16 @@ st.title("InvestorGPT")
 
 st.markdown(
     """
+    Welcome to InvestorGPT
     
+    Write down the name of a company and our Agent will do the research for you.
     """
 )
+
+company = st.text_input("Write the name of the company you are interested on.")
+
+if company:
+    result = agent.invoke(
+        f"Give me information on {company}'s stock, considering it's finalcials, income statements, stock performance and help me analyze if it's a potential good investment."
+    )
+    st.write(result["output"].replace("$", "\$"))
